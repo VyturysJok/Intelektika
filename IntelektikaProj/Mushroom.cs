354a5a383827b9764c91d3c6ebbe4e7603225f2f
Romas <rpromas@gmail.com>
2019-05-14 20:27:46 +0300

    Add project files.

---
+++ IntelektikaProj/Mushroom.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelektikaProj
+{
+    public class Mushroom
+    {
+        public bool IsEdible { get; set; }
+
+        public CapShape CapShapeValue { get; set; }
+        public CapSurface CapSurfaceValue { get; set; }
+        public CapColor CapColorValue { get; set; }
+
+        public Bruises BruisesValue { get; set; }
+        public Odor OdorValue { get; set; }
+
+        public GillAttachment GillAttachmentValue { get; set; }
+        public GillSpacing GillSpacingValue { get; set; }
+        public GillSize GillSizeValue { get; set; }
+        public GillColor GillColorValue { get; set; }
+
+        public StalkShape StalkShapeValue { get; set; }
+        public StalkRoot StalkRootValue { get; set; }
+        public StalkSurfaceAboveRing StalkSurfaceAboveRingValue { get; set; }
+        public StalkSurfaceBelowRing StalkSurfaceBelowRingValue { get; set; }
+        public StalkColorAboveRing StalkColorAboveRingValue { get; set; }
+        public StalkColorBelowRing StalkColorBelowRingValue { get; set; }
+
+        public VeilType VeilTypeValue { get; set; }
+        public VeilColor VeilColorValue { get; set; }
+
+        public RingNumber RingNumberValue { get; set; }
+        public RingType RingTypeValue { get; set; }
+
+        public SporePrintColor SporePrintColorValue { get; set; }
+        public Population PopulationValue { get; set; }
+        public Habitat HabitatValue { get; set; }
+
+
+        public Mushroom(string values)
+        {
+            ParseStringToVariables(values);
+        }
+
+        public Enum[] getEnums()
+        {
+            return new Enum[]
+            {
+                CapShapeValue,
+                CapSurfaceValue,
+                CapColorValue,
+                BruisesValue,
+                OdorValue,
+                GillAttachmentValue,
+                GillSpacingValue,
+                GillSizeValue,
+                GillColorValue,
+                StalkShapeValue,
+                StalkRootValue,
+                StalkSurfaceAboveRingValue,
+                StalkSurfaceBelowRingValue,
+                StalkColorAboveRingValue,
+                StalkColorBelowRingValue,
+                VeilTypeValue,
+                VeilColorValue,
+                RingNumberValue,
+                RingTypeValue,
+                SporePrintColorValue,
+                PopulationValue,
+                HabitatValue
+            };
+        }
+
+        public void ParseStringToVariables(string values)
+        {
+            var parsedValues = values.Replace(",", string.Empty);
+
+            IsEdible = parsedValues[0] == 'e';
+
+            CapShapeValue = (CapShape) parsedValues[1];
+            CapSurfaceValue = (CapSurface) parsedValues[2];
+            CapColorValue = (CapColor) parsedValues[3];
+
+            BruisesValue = (Bruises) parsedValues[4];
+            OdorValue = (Odor) parsedValues[5];
+
+            GillAttachmentValue = (GillAttachment) parsedValues[6];
+            GillSpacingValue = (GillSpacing) parsedValues[7];
+            GillSizeValue = (GillSize) parsedValues[8];
+            GillColorValue = (GillColor) parsedValues[9];
+
+            StalkShapeValue = (StalkShape) parsedValues[10];
+            StalkRootValue = (StalkRoot) parsedValues[11];
+            StalkSurfaceAboveRingValue = (StalkSurfaceAboveRing) parsedValues[12];
+            StalkSurfaceBelowRingValue = (StalkSurfaceBelowRing) parsedValues[13];
+            StalkColorAboveRingValue = (StalkColorAboveRing) parsedValues[14];
+            StalkColorBelowRingValue = (StalkColorBelowRing) parsedValues[15];
+
+            VeilTypeValue = (VeilType) parsedValues[16];
+            VeilColorValue = (VeilColor) parsedValues[17];
+
+            RingNumberValue = (RingNumber) parsedValues[18];
+            RingTypeValue = (RingType) parsedValues[19];
+
+            SporePrintColorValue = (SporePrintColor) parsedValues[20];
+            PopulationValue = (Population) parsedValues[21];
+            HabitatValue = (Habitat) parsedValues[22];
+
+        }
+
+        public enum CapShape  
+        {
+            bell = 'b',
+            conical = 'c',
+            convex = 'x',
+            flat = 'f',
+            knobbed = 'k',
+            sunken = 's'
+        }
+
+        public enum CapSurface
+        {
+            fibrous = 'f',
+            grooves = 'g',
+            scaly = 'y',
+            smooth = 's'
+        }
+
+        public enum CapColor
+        {
+            brown = 'n',
+            buff = 'b',
+            cinnamon = 'c',
+            gray = 'g',
+            green = 'r',
+            pink = 'p',
+            purple = 'u',
+            red = 'e',
+            white = 'w',
+            yellow = 'y'
+        }
+
+        public enum Bruises
+        {
+            bruises = 't',
+            no = 'f'
+        }
+
+        public enum Odor
+        {
+            almond = 'a',
+            anise = 'l',
+            creosote = 'c',
+            fishy = 'y',
+            foul = 'f',
+            musty = 'm',
+            none = 'n',
+            pungent = 'p',
+            spicy = 's'
+        }
+
+        public enum GillAttachment
+        {
+            attached = 'a',
+            descending = 'd',
+            free = 'f',
+            notched = 'n'
+        }
+
+        public enum GillSpacing
+        {
+            close = 'c',
+            crowded = 'w',
+            distant = 'd'
+        }
+
+        public enum GillSize
+        {
+            broad = 'b',
+            narrow = 'n'
+        }
+
+        public enum GillColor
+        {
+            black = 'k',
+            brown = 'n',
+            buff = 'b',
+            chocolate = 'h',
+            gray = 'g',
+            green = 'r',
+            orange = 'o',
+            pink = 'p',
+            purple = 'u',
+            red = 'e',
+            white = 'w',
+            yellow = 'y'
+        }
+
+        public enum StalkShape
+        {
+            enlarging = 'e',
+            tapering = 't'
+        }
+
+        public enum StalkRoot
+        {
+            bulbous = 'b',
+            club = 'c',
+            cup = 'u',
+            equal = 'e',
+            rhizomorphs = 'z',
+            rooted = 'r',
+            missing = '?'
+        }
+
+        public enum StalkSurfaceAboveRing
+        {
+            fibrous = 'f',
+            scaly = 'y',
+            silky = 'k',
+            smooth = 's'
+        }
+
+        public enum StalkSurfaceBelowRing
+        {
+            fibrous = 'f',
+            scaly = 'y',
+            silky = 'k',
+            smooth = 's'
+        }
+
+        public enum StalkColorAboveRing
+        {
+            brown = 'n',
+            buff = 'b',
+            cinnamon = 'c',
+            gray = 'g',
+            orange = 'o',
+            pink = 'p',
+            red = 'e',
+            white = 'w',
+            yellow = 'y'
+        }
+
+        public enum StalkColorBelowRing
+        {
+            brown = 'n',
+            buff = 'b',
+            cinnamon = 'c',
+            gray = 'g',
+            orange = 'o',
+            pink = 'p',
+            red = 'e',
+            white = 'w',
+            yellow = 'y'
+        }
+
+        public enum VeilType
+        {
+            partial = 'p',
+            universal = 'u'
+        }
+
+        public enum VeilColor
+        {
+            brown = 'n',
+            orange = 'o',
+            white = 'w',
+            yellow = 'y'
+        }
+
+        public enum RingNumber
+        {
+            none = 'n',
+            one = 'o',
+            two = 't'
+        }
+
+        public enum RingType
+        {
+            cobwebby = 'c',
+            evanescent = 'e',
+            flaring = 'f',
+            large = 'l',
+            none = 'n',
+            pendant = 'p',
+            sheathing = 's',
+            zone = 'z'
+        }
+
+        public enum SporePrintColor
+        {
+            black = 'k',
+            brown = 'n',
+            buff = 'b',
+            chocolate = 'h',
+            green = 'r',
+            orange = 'o',
+            purple = 'u',
+            white = 'w',
+            yellow = 'y'
+        }
+
+        public enum Population
+        {
+            abundant = 'a',
+            clustered = 'c',
+            numerous = 'n',
+            scattered = 's',
+            several = 'v',
+            solitary = 'y'
+        }
+
+        public enum Habitat
+        {
+            grasses = 'g',
+            leaves = 'l',
+            meadows = 'm',
+            paths = 'p',
+            urban = 'u',
+            waste = 'w',
+            woods = 'd'
+        }
+    }
+}
---
+++ IntelektikaProj/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+
+namespace IntelektikaProj
+{
+    class Program
+    {
+        const decimal defaultProbabilityMain = 0.4M;
+        const decimal neutralProbabilityMain = 0.5M;
+        const decimal isSpamBoundaryMain = 0.6M;
+
+        private static readonly HttpClient _client = new HttpClient();
+        private const string BaseApiUrl = "https://www.kaggle.com/api/v1/";
+        private const string CompetitionName = "uciml/mushroom-classification";
+
+        private static Dictionary<Enum, int> PoisonousCount = new Dictionary<Enum, int>();
+        private static Dictionary<Enum, int> EdibleCount = new Dictionary<Enum, int>();
+        private static Dictionary<Enum, int> Counts = new Dictionary<Enum, int>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kaggle Api C# example.");
+            var auth = new { Username = string.Empty, Key = string.Empty };
+
+            Console.WriteLine("Creating output folder ...");
+            if (Directory.Exists(CompetitionName))
+            {
+                Directory.Delete(CompetitionName, true);
+            }
+
+            Directory.CreateDirectory(CompetitionName);
+
+            Console.WriteLine("Loading api auth information ...");
+            using (var reader = new StreamReader("kaggle.json"))
+            {
+                var json = reader.ReadToEnd();
+                auth = JsonConvert.DeserializeAnonymousType(json, auth);
+            }
+            Console.WriteLine($"username: {auth.Username}, password: {auth.Key}", auth);
+            var authToken = Convert.ToBase64String(
+                System.Text.ASCIIEncoding.ASCII.GetBytes(
+                    string.Format($"{auth.Username}:{auth.Key}", auth)
+                ));
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authToken);
+
+            var data = ParseData();
+
+            foreach (var shroom in data)
+            {
+                var enums = shroom.getEnums();
+
+                foreach (var shroomClassifier in enums)
+                {
+                    if (shroom.IsEdible)
+                    {
+                        if (EdibleCount.ContainsKey(shroomClassifier))
+                        {
+                            EdibleCount[shroomClassifier] = EdibleCount[shroomClassifier] + 1;
+                        }
+                        else
+                        {
+                            EdibleCount[shroomClassifier] = 1;
+                        }
+                    }
+
+                    else
+                    {
+                        if (PoisonousCount.ContainsKey(shroomClassifier))
+                        {
+                            PoisonousCount[shroomClassifier] = PoisonousCount[shroomClassifier] + 1;
+                        }
+                        else
+                        {
+                            PoisonousCount[shroomClassifier] = 1;
+                        }
+                    }
+
+                    if (Counts.ContainsKey(shroomClassifier))
+                    {
+                        Counts[shroomClassifier] = Counts[shroomClassifier] + 1;
+                    }
+                    else
+                    {
+                        Counts[shroomClassifier] = 1;
+                    }
+                }
+            }
+
+            var a = PoisonousCount;
+            var b = EdibleCount;
+            var bbz = getWordSpamProbabilityTable(PoisonousCount, EdibleCount, Counts);
+        }
+
+        static Dictionary<Enum, decimal> getWordSpamProbabilityTable(Dictionary<Enum, int> poisonousClassifierCounts, Dictionary<Enum, int> edibleClassifierCounts, Dictionary<Enum, int> allClassifierCounts)
+        {
+            Dictionary<Enum, decimal> poisonousProbabilityTable = new Dictionary<Enum, decimal>();
+
+            foreach (var classifier in allClassifierCounts.Keys)
+            {
+                int poisonousRepeatCount = poisonousClassifierCounts.ContainsKey(classifier) ? poisonousClassifierCounts[classifier] : 0;
+                int edibleRepeatCount = edibleClassifierCounts.ContainsKey(classifier) ? edibleClassifierCounts[classifier] : 0;
+                int allRepeatCount = allClassifierCounts.ContainsKey(classifier) ? allClassifierCounts[classifier] : 0;
+                if (allRepeatCount == 0) {
+                    continue;
+                }
+                decimal Pclassifierpoisonous = (decimal)poisonousRepeatCount / allRepeatCount;
+                decimal Pclassifieredible = (decimal)edibleRepeatCount / allRepeatCount;
+                decimal PclassifierWord = defaultProbabilityMain;
+                if (Pclassifieredible > 0.0M && Pclassifierpoisonous == 0.0M)
+                {
+                    PclassifierWord = 0.01M;
+                }
+                else if (Pclassifierpoisonous > 0.0M && Pclassifieredible == 0.0M)
+                {
+                    PclassifierWord = 0.99M;
+                }
+                else if (Pclassifierpoisonous > 0 && Pclassifieredible > 0)
+                {
+                    PclassifierWord = Pclassifierpoisonous / (Pclassifierpoisonous + Pclassifieredible);
+                }
+                else
+                {
+                    PclassifierWord = 0.01M;
+                }
+
+                poisonousProbabilityTable.Add(classifier, PclassifierWord);
+            }
+            return poisonousProbabilityTable;
+        }
+
+
+        static List<Mushroom> ParseData()
+        {
+            List<Mushroom> result = new List<Mushroom>();
+            var getTask = _client.GetStringAsync(BaseApiUrl + "datasets/download/uciml/mushroom-classification/mushrooms.csv");
+            var json = getTask.Result;
+
+            string[] lines = json.Split('\n').Skip(1).ToArray();
+
+            foreach (var line in lines)
+            {
+                result.Add(new Mushroom(line));
+            }
+
+            return result;
+        }
+
+        static List<string> ListCompetitionFiles()
+        {
+            var getTask = _client.GetStringAsync(BaseApiUrl + "datasets/list/" + CompetitionName);
+            var json = getTask.Result;
+            var list = new List<string>();
+            dynamic files = JsonConvert.DeserializeObject<dynamic[]>(json);
+
+            foreach (dynamic file in files)
+            {
+                // fields, case senstive
+                // ref
+                // description
+                // name
+                // totalBytes
+                // url - this is not the api download url, do not use this
+                // creationDate
+                Console.WriteLine($"{file.name}", file);
+                list.Add(file.name.ToString());
+            }
+
+            return list;
+        }
+
+        static void DownloadCompetitionFiles(IEnumerable<string> files)
+        {
+            foreach (var file in files)
+            {
+                Console.WriteLine($"downloading {file} ...", file);
+                var filename = CompetitionName + "/" + file;
+                var getTask = _client.GetStreamAsync(BaseApiUrl + "competitions/data/download/" + filename);
+                using (var stream = getTask.Result)
+                using (var output = new FileStream(filename, FileMode.CreateNew))
+                {
+                    stream.CopyTo(output);
+                }
+            }
+        }
+    }
+}
